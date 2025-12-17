5b7c374ea56dd5d0697681250eff274afcdbb610
Hafifahussein <hafifahussein5@gmail.com>
2025-12-17 23:06:23 +0300

    Deploy-vercel

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,73 +1,20 @@
 const CACHE_NAME = 'moodflow-v1'
-const urlsToCache = [
-  '/',
-  '/static/js/main.chunk.js',
-  '/static/js/bundle.js',
-  '/static/css/main.chunk.css',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png',
-  '/splash-icon.png',
-]
 
-// Install event
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('Opened cache')
-      return cache.addAll(urlsToCache)
+      return cache.addAll([
+        '/',
+        '/static/js/main.chunk.js',
+        '/static/js/bundle.js',
+        '/manifest.json',
+      ])
     }),
   )
 })
 
-// Fetch event
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches
-      .match(event.request)
-      .then((response) => {
-        // Cache hit - return response
-        if (response) {
-          return response
-        }
-
-        // Clone the request
-        const fetchRequest = event.request.clone()
-
-        return fetch(fetchRequest).then((response) => {
-          // Check if valid response
-          if (!response || response.status !== 200 || response.type !== 'basic') {
-            return response
-          }
-
-          // Clone the response
-          const responseToCache = response.clone()
-
-          caches.open(CACHE_NAME).then((cache) => {
-            cache.put(event.request, responseToCache)
-          })
-
-          return response
-        })
-      })
-      .catch(() => {
-        // If both cache and network fail, show offline page
-        return caches.match('/')
-      }),
-  )
-})
-
-// Activate event
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName)
-          }
-        }),
-      )
-    }),
+    caches.match(event.request).then((response) => response || fetch(event.request)),
   )
 })
